24cd148e36710a80f1fe60bb6089c9d022a9e736
farhanmian <farhanmian099@gmail.com>
2024-10-30 14:50:38 +0530

    working on vCardForm

---
+++ src/components/app/Forms/VCard/VCardForm.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const VCardForm = () => {
+  return (
+    <form>
+      <div className="mb-5">
+        <input
+          placeholder="Name of the QR Code"
+          className="text-xl px-3 py-2"
+        />
+      </div>
+
+      <div>
+        
+      </div>
+    </form>
+  );
+};
+
+export default VCardForm;
